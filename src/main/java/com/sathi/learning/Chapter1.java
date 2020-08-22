33f7aa837534b8cdde607e70c0c578f268c13674
sm611316 <sathi.mandadi@broadcom.com>
2020-08-23 01:37:07 +0530

    spell

--- src/main/java/com/sathi/learning/Chapter1.java
+++ src/main/java/com/sathi/learning/Chapter1.java
@@ -4,7 +4,7 @@ public class Chapter1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My Weight on mooon is: " + 0.17 * 62 + "\n");
+		System.out.println("My Weight on moon is: " + 0.17 * 62 + "\n");
 		for (int i = 1; i < 145; i++) {
 			System.out.println(i + " Inches = " + i / 39.37 + "Meters");
 			if (i % 12 == 0 & i > 1) {
